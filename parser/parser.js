7b6c1f948f8522e1304ec075989d38ed8c3e94a5
ccckmit <ccckmit@gmail.com>
2015-11-02 19:36:16 +0800

    modify parser.js add try error

--- parser/parser.js
+++ parser/parser.js
@@ -4,23 +4,36 @@
 var camxes = require("./camxes");
 
 function parse(text) {
-  return camxes.parse(text);
+  var tree, error;
+  try {
+    tree=camxes.parse(text);
+  } catch (e) {
+    error = e;
+  }
+  return { tree:tree, error:error } ;
 }
 
-var tree=parse(process.argv[2]);
-console.log("%j", tree);
+var p=parse(process.argv[2]);
+console.log("%j", p);
 
 /*
-D:\Dropbox\cccwd\web\lowan\parser>node parser "mi klama vi vu"
-["text",["text_1",["paragraphs",["paragraph",["statement",["statement_1",["state
-ment_2",["statement_3",["sentence",[["terms",["terms_1",["terms_2",["term",["ter
-m_1",["sumti",["sumti_1",["sumti_2",["sumti_3",["sumti_4",["sumti_5",["sumti_6",
-["KOhA_clause",[["KOhA","mi"]]]]]]]]]]]]]]]],["bridi_tail",["bridi_tail_1",["bri
-di_tail_2",["bridi_tail_3",["selbri",["selbri_1",["selbri_2",["selbri_3",["selbr
-i_4",["selbri_5",["selbri_6",["tanru_unit",["tanru_unit_1",["tanru_unit_2",["BRI
-VLA_clause",[["BRIVLA",["gismu","klama"]]]]]]]]]]]]]],["tail_terms",["terms",["t
-erms_1",["terms_2",["term",["term_1",["tag",["tense_modal",["simple_tense_modal"
-,[["space",["VA_clause",["VA","vi"]]]]]]]]]]],["terms_1",["terms_2",["term",["te
-rm_1",["tag",["tense_modal",["simple_tense_modal",[["space",["VA_clause",["VA","
-vu"]]]]]]]]]]]]]]]]]]]]]]]]]]
+D:\Dropbox\cccwd\web\lowan\parser>node parser "mi klama klama mi vu"
+{"tree":["text",["text_1",["paragraphs",["paragraph",["statement",["statement_1"
+,["statement_2",["statement_3",["sentence",[["terms",["terms_1",["terms_2",["ter
+m",["term_1",["sumti",["sumti_1",["sumti_2",["sumti_3",["sumti_4",["sumti_5",["s
+umti_6",["KOhA_clause",[["KOhA","mi"]]]]]]]]]]]]]]]],["bridi_tail",["bridi_tail_
+1",["bridi_tail_2",["bridi_tail_3",["selbri",["selbri_1",["selbri_2",["selbri_3"
+,["selbri_4",["selbri_5",["selbri_6",["tanru_unit",["tanru_unit_1",["tanru_unit_
+2",["BRIVLA_clause",[["BRIVLA",["gismu","klama"]]]]]]]]]],["selbri_4",["selbri_5
+",["selbri_6",["tanru_unit",["tanru_unit_1",["tanru_unit_2",["BRIVLA_clause",[["
+BRIVLA",["gismu","klama"]]]]]]]]]]]]]],["tail_terms",["terms",["terms_1",["terms
+_2",["term",["term_1",["sumti",["sumti_1",["sumti_2",["sumti_3",["sumti_4",["sum
+ti_5",["sumti_6",["KOhA_clause",[["KOhA","mi"]]]]]]]]]]]]]],["terms_1",["terms_2
+",["term",["term_1",["tag",["tense_modal",["simple_tense_modal",[["space",["VA_c
+lause",["VA","vu"]]]]]]]]]]]]]]]]]]]]]]]]]]}
+
+D:\Dropbox\cccwd\web\lowan\parser>node parser "mi klama klama mi vu le"
+{"error":{"name":"SyntaxError","expected":[],"found":"l","message":"Expected end
+ of input but \"l\" found.","offset":21,"line":1,"column":22}}
+
 */ 
